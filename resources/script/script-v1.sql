2de3f74146c4e0b6ffe5da4d1771b33fd55ac9c2
Diego10Rocha <diego93rocha@gmail.com>
2025-10-19 08:36:36 -0300

    adição da tabela de usuário e modificações nas demais tabelas

--- resources/script/script-v1.sql
+++ resources/script/script-v1.sql
@@ -1,7 +1,13 @@
 CREATE TABLE area_pesquisa (
 	id uuid PRIMARY KEY,
 	nome varchar(100) not null,
-	descricao text
+	descricao text,
+	criado_por varchar(100)/*pensei em salvar o id de quem fez o cadastro*/
+);
+
+CREATE TABLE curso (
+	id uuid primary key,
+	nome varchar(45) not null
 );
 
 CREATE TABLE professor (
@@ -9,8 +15,11 @@ CREATE TABLE professor (
 	nome varchar(75) not null,
 	email varchar(100) unique,
 	telefone varchar(11) unique,
-	curso varchar(45) not null,
-	departamento varchar(45)
+	id_curso uuid not null,
+	departamento varchar(45),
+	criado_por varchar(100),/*pensei em salvar o id de quem fez o cadastro*/
+	
+	FOREIGN KEY (id_curso) REFERENCES curso(id)
 );
 
 CREATE TABLE linha_pesquisa (
@@ -18,6 +27,7 @@ CREATE TABLE linha_pesquisa (
 	nome varchar(255),
 	descricao text,
 	id_area_pesquisa uuid,
+	criado_por varchar(100),/*pensei em salvar o id de quem fez o cadastro*/
 	
 	FOREIGN KEY (id_area_pesquisa) REFERENCES area_pesquisa(id)
 );
@@ -44,6 +54,22 @@ CREATE TABLE professor_has_area_interesse (
 	
 	FOREIGN KEY (area_pesquisa) REFERENCES area_pesquisa(id) 
 	ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TYPE tipo_usuario AS ENUM ('SUPER', 'DA');
+
+CREATE TABLE usuario (
+	id uuid primary key,
+	nome varchar(100) not null,
+	email varchar(45) not null unique,
+	senha varchar(100) not null,
+	ativo bool not null,
+	data_criacao timestamp,
+	data_atualizacao timestamp,
+	tipo_permissao tipo_usuario not null,
+	id_curso uuid,
+	
+	FOREIGN KEY (id_curso) REFERENCES curso(id)
 );
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
